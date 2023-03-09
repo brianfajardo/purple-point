bc4b331e3e132f109a93e1c1b02ef0d658e5a4fb
Brian Fajardo <brian@breathelife.com>
2023-03-09 00:39:01 -0500

    run format script on codebase

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,15 +1,26 @@
 module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  trailingComma: "none",
+  singleQuote: false,
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+
   plugins: [
-    require.resolve('prettier-plugin-astro'),
-    require('prettier-plugin-tailwindcss')
+    require.resolve("prettier-plugin-astro"),
+    require("prettier-plugin-tailwindcss")
   ],
+  pluginSearchDirs: false,
+
   overrides: [
     {
-      files: '*.astro',
+      files: "*.astro",
       options: {
-        parser: 'astro',
-      },
-    },
-  ],
-  pluginSearchDirs: false
-};
+        parser: "astro"
+      }
+    }
+  ]
+}
